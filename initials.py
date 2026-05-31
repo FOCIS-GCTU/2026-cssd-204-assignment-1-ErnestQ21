2651ef1b9bf493de947202a0da6c21152273f54a
ErnestQ21 <2425403363@live.gctu.edu.gh>
2026-05-31 15:55:29 +0000

    Add files via upload
    Submit assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Assignment 1 - printing out my initials QEJ in block letters
+# Assignment Number: 1
+# 
+# Name: Quaye Ernest Junior
+# STUDENT ID:  2425403363
+# Email: 2425403363@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+# 
+# On my honor, Quaye Ernest Junior, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # just printing the small initials first with a blank line before and after
+    print()
+    print("...QEJ")
+    print()
 
+    # printing the 10x12 block letters line by line
+    print("....QQQQQQQQ" + "......." + "EEEEEEEEEEEE" + "......" + "JJJJJJJJJJJJ...")
+    print("...QQQQQQQQQQ" + "......" + "EEEEEEEEEEEE" + "......" + "JJJJJJJJJJJJ...")
+    print("...QQ......QQ" + "......" + "EEEE" + "......................" + "JJJJ...")
+    print("...QQ......QQ" + "......" + "EEEE" + "......................" + "JJJJ...")
+    print("...QQ......QQ" + "......" + "EEEEEEEEEE" + "................" + "JJJJ...")
+    print("...QQ....Q.QQ" + "......" + "EEEEEEEEEE" + "................" + "JJJJ...")
+    print("...QQ.....QQQ" + "......" + "EEEE" + "......................" + "JJJJ...")
+    print("...QQQQQQQQQQ" + "......" + "EEEE" + ".............." + "JJJJ...." + "JJJJ...")
+    print("....QQQQQQQQ..QQ" + "..." + "EEEEEEEEEEEE" + "...**.." + "JJJJJJJJJJJ..." + "**")
+    print("..........QQQQQ" + "...." + "EEEEEEEEEEEE" + "...**..." + "JJJJJJJJJ...." + "**")
+
+    print()
 
 
 main()
